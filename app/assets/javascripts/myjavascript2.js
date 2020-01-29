c6debd9204e54da30b15df09b7eceeea68b30314
WilliamJones62 <williamjones120862@gmail.com>
2020-01-29 14:09:19 -0500

    add ajax to empty page to find orders

--- app/assets/javascripts/myjavascript2.js
+++ app/assets/javascripts/myjavascript2.js
@@ -14,3 +14,25 @@ function setQuit() {
     z.style.display = 'none';
     }
 }
+
+function showOrders() {
+  //this is set to the XMLHttpRequest object that fired the event
+  var y = document.getElementById("refresh");
+  var z = document.getElementById("message");
+  var repos = JSON.parse(this.responseText);
+  if (repos.length == 0) {
+    y.style.display = 'none';
+    z.innerHTML = 'There are no outstanding orders at this time.';
+  } else {
+    y.style.display = 'inline';
+    z.innerHTML = 'There are outstanding orders. Click the refresh button below to process new orders.';
+  }
+}
+
+function getOrders() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showOrders);
+  // req.open('GET', 'http://fsorders.dartagnan.com/api/v1/fs_orders/api.json');
+  req.open('GET', 'http://fsorders.dartagnan.com/api/v1/fs_orders/api.json');
+  req.send();
+}
